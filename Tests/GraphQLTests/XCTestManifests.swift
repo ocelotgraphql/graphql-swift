2cbd5ff7b3a6b1324234ffa3c0f89116950c8223
Moritz Lang <hi@slashmo.codes>
2018-11-14 18:12:19 +0100

    Update LinuxMain.swift

--- Tests/GraphQLTests/XCTestManifests.swift
+++ Tests/GraphQLTests/XCTestManifests.swift
@@ -6,10 +6,17 @@ extension GraphQLErrorTestCase {
     ]
 }
 
+extension LexerPunctuationTokenTestCase {
+    static let __allTests = [
+        ("testItLexesBangs", testItLexesBangs),
+    ]
+}
+
 #if !os(macOS)
 public func __allTests() -> [XCTestCaseEntry] {
     return [
         testCase(GraphQLErrorTestCase.__allTests),
+        testCase(LexerPunctuationTokenTestCase.__allTests),
     ]
 }
 #endif
